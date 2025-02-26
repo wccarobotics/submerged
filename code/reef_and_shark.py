b8dc24be79211b75b073ee5c740408f8008875f3
Nathaneal Dessource <nathanealdessource@student.flcoe.edu>
2025-02-26 10:08:08 -0500

    Add reef / shark mission

---
+++ code/reef_and_shark.py
@@ -0,0 +1,31 @@
+from base_robot import *
+
+# Copy this text into a new mission file. Name it something like
+# r02_unexpected_encounter.  Don't use any spaces or punctuation,
+# other than _. The name MUST end with .py
+#
+# Add good comments, such as what the mission is supposed to do,
+# how to align the robot in home, any initial starting instructions,
+# such as how it should be loaded with anything, arm positions, etc.
+#
+# These existing comments can be deleted
+
+
+def Run(br: BaseRobot):
+    br.driveArcDist(mm(40), mm(29))
+    br.waitForMillis(80)
+    br.turnInPlace(-134)
+    br.waitForMillis(70)
+    br.driveForDistance(mm(5), speedPct=80)
+    br.waitForMillis(60)
+    br.driveForDistance(mm(-5), speedPct=-80)
+    br.waitForMillis(60)
+    br.turnInPlace(49)
+    br.waitForMilli(60)
+    br.driveForDistance(mm(9), speedPct=100)
+    br.waitForMillis(60)
+
+# It runs the Run method if this file is run directly (not from the master program)
+if __name__ == "__main__":
+    br = BaseRobot()
+    Run(br)
