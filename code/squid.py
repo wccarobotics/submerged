e2251608855e934112a3ea52b2347ab416cf2642
Natalia Chavez-Garcia <nataliachavezgarcia1@student.flcoe.edu>
2025-02-26 10:04:38 -0500

    Add squid program

---
+++ code/squid.py
@@ -0,0 +1,34 @@
+from base_robot import *
+
+# Copy this text into a new mission file. Name it something like
+# r02_unexpected_encounter.  Don't use any spaces or punctuation,
+# other than _. The name MUST end with .py
+#
+# Add good comments, such as what the mission is supposed to do,
+# how to align the robot in home, any initial starting instructions,
+# such as how it should be loaded with anything, arm positions, etc.
+#
+# These existing comments can be deleted
+
+
+def Run(br: BaseRobot):
+
+    # Your mission code goes here, step-by-step
+    # It MUST be indented just like the lines below
+
+    br.curve(mm(15), -45, speedPct=50)
+
+    br.driveForDistance(mm(12), speedPct=-50)
+    br.driveForDistance(mm(-12), speedPct=-50)
+
+
+
+    
+ 
+
+
+# Don't modify the code below
+# It runs the Run method if this file is run directly (not from the master program)
+if __name__ == "__main__":
+    br = BaseRobot()
+    Run(br)
