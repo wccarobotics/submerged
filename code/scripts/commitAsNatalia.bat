1a48a4f1edd16f3a6d265df9af76dc09d3a1d965
Daniel Plaisted <dsplaisted@gmail.com>
2025-02-26 10:03:02 -0500

    Add more commit scripts

---
+++ code/scripts/commitAsNatalia.bat
@@ -0,0 +1,2 @@
+git config user.name "Natalia Chavez-Garcia"
+git config user.email nataliachavezgarcia1@student.flcoe.edu
---
+++ code/scripts/commitAsNathaneal.bat
@@ -0,0 +1,2 @@
+git config user.name "Nathaneal Dessource"
+git config user.email nathanealdessource@student.flcoe.edu
