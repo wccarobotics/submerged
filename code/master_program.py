47dd6089546fc1bafd46817b210165c71db54798
Lucas Plaisted <lsplaisted@outlook.com>
2025-03-11 09:07:55 -0400

    updated run order

--- code/master_program.py
+++ code/master_program.py
@@ -8,7 +8,6 @@ import reef_and_shark, raise_mast, board_cross, squid, squid_delivery, angler, r
 br = BaseRobot()
 
 pressed = []
-# col = br.colorSensor.color()
 run_number = 0
 
 runs = {
@@ -16,10 +15,10 @@ runs = {
     1: raise_mast.Run,
     2: board_cross.Run,
     3: squid.Run,
-    4: squid_delivery.Run,
-    5: angler.Run,
-    6: r06_change_shipping_lanes.Run,
-    7: send_over_submersible.Run,
+    4: angler.Run,
+    5: r06_change_shipping_lanes.Run,
+    6: send_over_submersible.Run,
+    7: squid_delivery.Run,
     8: crabz.Run,
 }
 br.hub.system.set_stop_button([Button.CENTER, Button.BLUETOOTH])
@@ -42,65 +41,3 @@ while True:
     br.hub.light.on(Color.BLUE)
     br.hub.display.number(run)
     wait(150)
-
-    # while True:
-    #     col = br.colorSensor.color()
-    #     # The first thing this program does is it detects what color is
-    #     # being help up to the robot color sensor.
-    #     # If no color is detected, then it will display a sad face
-    #     if col == Color.SENSOR_NONE:
-    #         br.hub.display.icon(Icon.SAD)
-    #         br.hub.light.on(Color.RED)
-    #     else:  #  If a color is detected, then it will display a happy face
-    #         br.hub.display.icon(Icon.HAPPY)
-    #         br.hub.light.on(br.myColor2DefaultColorDict[col])
-
-    #     wait(50)
-    #     pressed = br.hub.buttons.pressed()
-    #     #  When the left button is pressed, it will break out of the loop
-    #     if Button.LEFT in pressed:
-    #         break
-    #     if Button.BLUETOOTH in pressed:
-    #         # If the Bluetooth button is pressed, it will run the motors fast for
-    #         # cleaning
-    #         br.leftDriveMotor.run(1000)
-    #         br.rightDriveMotor.run(1000)
-    #         wait(25000)
-    #         br.leftDriveMotor.brake()
-    #         br.rightDriveMotor.brake()
-
-    # # It will now launch the mission coresponding to the color
-    # if col == Color.SENSOR_YELLOW:
-    #     print("Launching Yellow")
-    #     noah2.Run(br)
-    #     br.waitForForwardButton()
-
-    #     shaila.Run(br)
-
-    # if col == Color.SENSOR_GREEN:
-    #     print("Launching Green")
-    #     GiosToast.Run(br)
-
-    # if col == Color.SENSOR_LIME:
-    #     print("Launching Lime")
-    #     noahsdice.Run(br)
-
-    # # if col == Color.SENSOR_MAGENTA:
-    # #     print("Launching Magenta")
-    # #     shaila.Run(br)
-
-    # if col == Color.SENSOR_WHITE:
-    #     print("Launching White")
-    #     shaila2.Run(br)
-
-    # if col == Color.SENSOR_ORANGE:
-    #     print("Launching Orange")
-    #     noah4.Run(br)
-
-    # if col == Color.SENSOR_RED or col == Color.SENSOR_MAGENTA:
-    #     print("Launching Red/Magenta")
-    #     carternoah.Run(br)
-
-    # if col == Color.SENSOR_BLUE:
-    #     print("Launching Blue")
-    #     Carovanni.Run(br)
