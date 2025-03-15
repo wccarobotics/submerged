6e7d397cad0f84bd46c13494b092216fe5d33e2a
Lucas Plaisted <lsplaisted@outlook.com>
2025-03-15 13:09:47 -0400

    changed bpm

--- code/celebrate.py
+++ code/celebrate.py
@@ -33,7 +33,7 @@ def Run(br: BaseRobot):
     )
     br.hub.display.icon(STAR)
     br.hub.light.animate((Color.RED, Color.YELLOW, Color.GREEN, Color.CYAN), 200)
-    br.hub.speaker.play_notes(['G4/4', 'G4/4', 'E4/8', 'G4/4', 'E4/8', 'G4/8', 'E4/8', 'D4/8', 'G4/4', 'R/8', 'R/8', 'G4/8', 'D5/8', 'E5/8', 'D5/8', 'E5/8', 'D5/4', 'C5/16', 'B4/16', 'A4/8', 'G3/4', 'A3/4', 'G3/4', 'G5/4'], 180)
+    br.hub.speaker.play_notes(['G4/4', 'G4/4', 'E4/8', 'G4/4', 'E4/8', 'G4/8', 'E4/8', 'D4/8', 'G4/4', 'R/8', 'R/8', 'G4/8', 'D5/8', 'E5/8', 'D5/8', 'E5/8', 'D5/4', 'C5/16', 'B4/16', 'A4/8', 'G3/4', 'A3/4', 'G3/4', 'G5/4'], 160)
     while True:
         pass
 # Don't modify the code below
