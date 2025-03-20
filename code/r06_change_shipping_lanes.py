57e7cf82989aa7f780e7d91c8bd1b098df9aca8b
Lucas Plaisted <lsplaisted@outlook.com>
2025-03-20 16:14:49 -0400

    fixed stuff?

--- code/r06_change_shipping_lanes.py
+++ code/r06_change_shipping_lanes.py
@@ -12,34 +12,41 @@ from base_robot import *
 
 
 def Run(br: BaseRobot):
-    # start on right side, facing north, on the first black notch from the edge of the mat
+    # start on right side, facing north, on the third small black notch from the edge of the mat
     # get to boat
-    br.driveForDistance(mm(13.7), 50)
+    initial_heading = br.hub.imu.heading()
+
+    br.driveForDistance(mm(13.5), 50)
     br.curve(mm(5), -45, 40)
 
     # rotate boat
     br.moveRightAttachmentMotorForDegrees(500, 30)
 
     # move atachment up(avoids getting caught on boat)
+    wait(500)
     br.moveRightAttachmentMotorForDegrees(-250, 100)
 
     # get to sonar
-    br.driveForDistance(mm(-2),30)
-    br.driveForDistance(mm(2),30)
-    br.driveForDistance(mm(9.7), 50)
-    br.curve(mm(5), -45, 40)
+    br.driveForDistance(mm(-2), 30)
+    br.driveForDistance(mm(2), 30)
+    br.driveForDistance(mm(9.7), 50, accelerationPct=40)
+    br.curve(mm(7), -30, 40)
+    br.driveForDistance(mm(4), 50)
 
     # do sonar
-    br.driveForDistance(mm(2), 50)
-    br.curve(mm(1), 80, 40)
-    br.driveForDistance(mm(5))
+    br.curve(mm(0), 75, 40)
+    br.driveForDistance(mm(5), 50)
     br.curve(0, 65, 50)
+    br.driveForDistance(mm(-3),20)
 
     # get to sample
-    br.driveForDistance(mm(-3),20)
-    br.curve(0, 57, 40)
-    br.driveForDistance(mm(14), 50)
-    
+    print("Initial heading: " + str(initial_heading))
+    print("Current heading: " + str(br.hub.imu.heading()))
+    turn_angle = ((initial_heading - br.hub.imu.heading())) + 75
+    print("Turn angle: " + str(turn_angle))
+    br.curve(0, turn_angle, 40)
+    br.driveForDistance(mm(15), 50)
+
     # return home
     br.driveForDistance(mm(-8), 100)
     br.curve(0, 80, 100)
