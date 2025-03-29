d4bc4b2c48456ae04af6cb03bf635c0ea1a01e1d
Lucas Plaisted <lsplaisted@outlook.com>
2025-03-29 16:31:14 -0400

    Delete boilerplate

--- code/r06_change_shipping_lanes.py
+++ code/r06_change_shipping_lanes.py
@@ -1,16 +1,5 @@
 from base_robot import *
 
-# Copy this text into a new mission file. Name it something like
-# r02_unexpected_encounter.  Don't use any spaces or punctuation,
-# other than _. The name MUST end with .py
-#
-# Add good comments, such as what the mission is supposed to do,
-# how to align the robot in home, any initial starting instructions,
-# such as how it should be loaded with anything, arm positions, etc.
-#
-# These existing comments can be deleted
-
-
 def Run(br: BaseRobot):
     # start on right side, facing north, on the third small black notch from the edge of the mat
     # get to boat
