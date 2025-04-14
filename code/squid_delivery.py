2dcb8b02f741a4c0dec62844ef9c89504a5aedad
Lucas Plaisted <lsplaisted@outlook.com>
2025-04-14 09:45:45 -0400

    worked on sample

--- code/squid_delivery.py
+++ code/squid_delivery.py
@@ -15,22 +15,25 @@ def Run(br: BaseRobot):
     # Your mission code goes here, step-by-step
     # It MUST be indented just like the lines below
     # The Robot must be aligned along the South Wall and againt the Square and Rectagle Aligners. Squid must be facing forward
+    initial_heading = br.hub.imu.heading()
 
     # get to mission model
-    br.driveForDistance(mm(3), 50)
-    br.curve(mm(5), -60, 25)
-    br.driveForDistance(mm(20), 50)
+    br.driveForDistance(mm(3), 50, then=Stop.NONE)
+    br.curve(mm(5), -55, 25, then=Stop.NONE)
+    br.driveForDistance(mm(23), 50, then=Stop.NONE)
     br.turnInPlace(45, 50)
     br.driveForDistance(mm(4), 50)
 
     # angler fish
-    br.curve(mm(2), -30, 50)
-    br. driveForDistance(mm(4), 30)
-    br.curve(mm(2), -25, 40)
-    br.driveForDistance(mm(4), 30)
-    br.driveForDistance(mm(-6), 50)
-    br.turnInPlace(-30)
-    br.driveForDistance(mm(-6))
+    br.turnInPlace(-45)
+    br.driveForDistance(mm(5), 40)
+    br.curve(mm(3), -35)
+    br.driveForDistance(mm(2), 50)
+    turn_angle = ((initial_heading - br.hub.imu.heading()-20))
+    br.curve(0, turn_angle, 40)
+    br.driveForDistance(mm(10), 50)
+    # br.moveRightAttachmentMotorForDegrees(300)
+    # br.driveForDistance(mm(-3), 50)
 
 # Don't modify the code below
 # It runs the Run method if this file is run directly (not from the master program)
