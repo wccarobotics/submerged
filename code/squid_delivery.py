ff1781a59b9fa6012f3cf0eff8a215897f6d6d3b
Joseph Caballero Castillo <josephcaballerocastillo@student.flcoe.edu>
2025-04-29 09:40:15 -0400

    working on it

--- code/squid_delivery.py
+++ code/squid_delivery.py
@@ -11,7 +11,7 @@ from base_robot import *
 # These existing comments can be deleted
 
 
-def Run(br: BaseRobot): 
+def Run(br: BaseRobot):
     # Your mission code goes here, step-by-step
     # It MUST be indented just like the lines below
     # The Robot must be aligned along the South Wall and againt the Square and Rectagle Aligners. Squid must be facing forward
@@ -20,8 +20,8 @@ def Run(br: BaseRobot):
     # get to mission model
     br.driveForDistance(mm(3), 50, then=Stop.NONE)
     br.curve(mm(5), -56, 25, then=Stop.NONE)
-    br.driveForDistance(mm(23), 50, then=Stop.NONE)
-    br.turnInPlace(45, 50)
+    br.driveForDistance(mm(22.5), 50, then=Stop.NONE)  # often tweaked
+    br.turnInPlace(47, 50)  # often tweaked
     br.driveForDistance(mm(5), 50)
 
     # angler fish
@@ -30,12 +30,17 @@ def Run(br: BaseRobot):
     br.curve(mm(2.8), -36)
     br.driveForDistance(mm(3.2), 50)
     br.driveForDistance(mm(-2))
-    turn_angle = ((initial_heading - br.hub.imu.heading()-20))
-    br.curve(0, turn_angle, 40)
+
+    # sample
+    turn_angle = initial_heading - br.hub.imu.heading() - 20
+    br.curve(0, turn_angle, 40)  # often tweaked
     br.driveForDistance(mm(10), 50)
     br.moveRightAttachmentMotorForDegrees(300)
-    br.driveArcDist(mm(10), mm(-6), -50, then=Stop.NONE)
-    br.driveForDistance(mm(-10))
+
+    # squid delivery
+    br.driveArcDist(mm(11), mm(-3), -50, then=Stop.NONE)  # often tweaked
+    br.driveForDistance(mm(-14))  # often tweaked
+
 
 # Don't modify the code below
 # It runs the Run method if this file is run directly (not from the master program)
