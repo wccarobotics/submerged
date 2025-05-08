bcbd223cbbf17fd17c3b74b2e68a720f1b2bedc1
Caleb West <calebwest@student.flcoe.edu>
2025-05-08 14:54:45 -0400

    started work on coral tree

---
+++ code/coral_tree.py
@@ -0,0 +1,26 @@
+from base_robot import *
+
+# Copy this text into a new mission file. Name it something like
+# r02_unexpected_encounter.  Don't use any spaces or punctuation,
+# other than _. The name MUST end with .py
+#
+# Add good comments, such as what the mission is supposed to do,
+# how to align the robot in home, any initial starting instructions,
+# such as how it should be loaded with anything, arm positions, etc.
+#
+# These existing comments can be deleted
+
+
+def Run(br: BaseRobot):
+    # get to tree
+    br.curve(mm(8.2), -90, then=Stop.NONE)
+    br.driveForDistance(mm(15), 50)
+    # raise arm
+    br.moveLeftAttachmentMotorForDegrees(-300, 1)
+
+
+# Don't modify the code below
+# It runs the Run method if this file is run directly (not from the master program)
+if __name__ == "__main__":
+    br = BaseRobot()
+    Run(br)
