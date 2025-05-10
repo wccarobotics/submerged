51cb411a30430236a97790cca490fcce5a0b65d4
Lucas Plaisted <lsplaisted@outlook.com>
2025-05-10 18:01:01 -0400

    WIP

--- code/squid_delivery.py
+++ code/squid_delivery.py
@@ -15,41 +15,67 @@ def Run(br: BaseRobot):
     # Your mission code goes here, step-by-step
     # It MUST be indented just like the lines below
     # The Robot must be aligned along the South Wall and againt the Square and Rectagle Aligners. Squid must be facing forward
+    
     initial_heading = br.hub.imu.heading()
-    br.curve(mm(4), -55)
-    br.driveForDistance(mm(15), speedPct=100)
-    turn_angle = initial_heading - br.hub.imu.heading() - 55
-    br.turnInPlace(turn_angle, 30)
-    br.driveForDistance(mm(5))
-    turn_angle = initial_heading - br.hub.imu.heading() - 55
-    br.turnInPlace(turn_angle, 30)
-    br.curve(mm(8), 27, speedPct=40)
-    br.driveForDistance(mm(1))
-    br.curve(mm(18), -30)
-    turn_angle = initial_heading - br.hub.imu.heading() - 55
-    br.turnInPlace(turn_angle, 30)
-    br.curve(mm(12), -10)
-    br.driveForDistance(mm(9))
-    turn_angle = initial_heading - br.hub.imu.heading() - 0
-    print(turn_angle)
-    br.turnInPlace(turn_angle, 30)
-    br.driveForMillis(1000, speedPct=20)
+    br.curve(mm(4), -48, then=Stop.NONE)
+    br.driveForDistance(mm(22), speedPct=50, then=Stop.NONE)
+    turn_angle = initial_heading - br.hub.imu.heading() - 48
+    br.turnInPlace(turn_angle, 30, then=Stop.NONE)
+    br.curve(mm(12), 15, speedPct=30, then=Stop.NONE)
+    br.curve(mm(12), -15, speedPct=30, then=Stop.NONE)
+    turn_angle = initial_heading - br.hub.imu.heading() - 50
+    br.turnInPlace(turn_angle, 30, then=Stop.NONE)
+    br.driveForDistance(mm(15), speedPct=50, then=Stop.NONE)
+    turn_angle = initial_heading - br.hub.imu.heading()
+    br.turnInPlace(turn_angle, 30, then=Stop.NONE)
+    br.driveForDistance(mm(3.5), speedPct=30, then=Stop.NONE)
     br.moveRightAttachmentMotorForDegrees(360)
     br.driveForDistance(mm(-3))
-    br.turnInPlace(90)
+    turn_angle = initial_heading - br.hub.imu.heading() + 20
+    br.turnInPlace(turn_angle, 30, then=Stop.NONE)
     br.moveRightAttachmentMotorForDegrees(-360)
-    br.driveForDistance(mm(5))
-    turn_angle = initial_heading - br.hub.imu.heading() + 10
-    br.turnInPlace(turn_angle, 30)
-    br.moveRightAttachmentMotorForDegrees(360, speedPct=25, wait=False)
     br.driveForMillis(500)
-    wait(2000)
-    br.driveForDistance(mm(-3))
-    turn_angle = initial_heading - br.hub.imu.heading() + 135
-    br.turnInPlace(turn_angle, 30)
-    br.driveForDistance(mm(6))
-    br.turnInPlace(-40)
-    br.driveForDistance(mm(-2))
+    br.turnInPlace(-97)
+    br.driveForMillis(500)
+    br.moveRightAttachmentMotorForDegrees(360)
+    br.waitForMillis(2000)
+    br.turnInPlace(150)
+    # br.moveRightAttachmentMotorForDegrees(360)
+    # br.waitForMillis(3000)
+    # br.curve(mm(4), -55, then=Stop.NONE)
+    # br.driveForDistance(mm(15), speedPct=100, then=Stop.NONE)
+    # turn_angle = initial_heading - br.hub.imu.heading() - 55
+    # br.turnInPlace(turn_angle, 30, then=Stop.NONE)
+    # br.driveForDistance(mm(5), then=Stop.NONE)
+    # turn_angle = initial_heading - br.hub.imu.heading() - 55
+    # br.turnInPlace(turn_angle, 30, then=Stop.NONE)
+    # br.curve(mm(8), 27, speedPct=40, then=Stop.NONE)
+    # br.driveForDistance(mm(1), then=Stop.NONE)
+    # br.curve(mm(18), -30, then=Stop.NONE)
+    # turn_angle = initial_heading - br.hub.imu.heading() - 55
+    # br.turnInPlace(turn_angle, 30, then=Stop.NONE)
+    # br.curve(mm(12), -10, then=Stop.NONE)
+    # br.driveForDistance(mm(9), then=Stop.NONE)
+    # turn_angle = initial_heading - br.hub.imu.heading() - 0
+    # print(turn_angle)
+    # br.turnInPlace(turn_angle, 30, then=Stop.NONE)
+    # br.driveForMillis(1000, speedPct=20, then=Stop.NONE)
+    # br.moveRightAttachmentMotorForDegrees(360)
+    # br.driveForDistance(mm(-3))
+    # br.turnInPlace(90)
+    # br.moveRightAttachmentMotorForDegrees(-360)
+    # br.driveForDistance(mm(5))
+    # turn_angle = initial_heading - br.hub.imu.heading() + 10
+    # br.turnInPlace(turn_angle, 30)
+    # br.moveRightAttachmentMotorForDegrees(360, speedPct=25, wait=False)
+    # br.driveForMillis(500)
+    # wait(2000)
+    # br.driveForDistance(mm(-3))
+    # turn_angle = initial_heading - br.hub.imu.heading() + 135
+    # br.turnInPlace(turn_angle, 30)
+    # br.driveForDistance(mm(6))
+    # br.turnInPlace(-40)
+    # br.driveForDistance(mm(-2))
 
 
 # Don't modify the code below
