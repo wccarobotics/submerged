7aecc04d779ba69633737c5461339ec3efe1be40
Lucas Plaisted <lsplaisted@outlook.com>
2025-06-05 10:36:19 -0400

    WIP

--- code/r06_change_shipping_lanes.py
+++ code/r06_change_shipping_lanes.py
@@ -8,7 +8,7 @@ def Run(br: BaseRobot):
 
     br.driveForDistance(mm(14), 50)
     br.curve(mm(3.5), -45, 40)
-    br.driveForDistance(mm(1), 10)
+    br.driveForDistance(mm(.5), 10)
 
     # rotate boat
     br.moveRightAttachmentMotorForDegrees(500, 30)
@@ -27,15 +27,18 @@ def Run(br: BaseRobot):
     br.curve(0, turn_angle, 40)
     br.driveForDistance(mm(10.7), 50, accelerationPct=40, then=Stop.NONE)
     br.curve(mm(7), -30, 40)
-    # br.driveForDistance(mm(-0.8), 50)
+    br.driveForDistance(mm(0.4), 50)
 
     # do sonar
     turn_angle = initial_heading - br.hub.imu.heading()
     br.curve(0, turn_angle, 40)
-    br.driveForDistance(mm(4.1), 20)
-    br.moveLeftAttachmentMotorForDegrees(4300, 100)
+    br.driveForDistance(mm(5.2), 10)
+    turn_angle = initial_heading - br.hub.imu.heading()
+    br.curve(0, turn_angle, 40)
+    br.moveLeftAttachmentMotorForMillis(5500, 100)
 
     # get to sample
+    br.driveForDistance(mm(-1))
     print("Initial heading: " + str(initial_heading))
     print("Current heading: " + str(br.hub.imu.heading()))
     turn_angle = ((initial_heading - br.hub.imu.heading())) + 88
@@ -43,11 +46,10 @@ def Run(br: BaseRobot):
     br.curve(0, turn_angle, 40)
     br.driveForDistance(mm(3), 50, then=Stop.NONE)
     br.moveLeftAttachmentMotorForDegrees(-1000, wait=False)
-    br.driveForDistance(mm(5), 20, then= Stop.NONE)
-    br.driveForMillis(500, 80)
+    br.driveForMillis(1000, 50)
 
     # return home
-    br.driveForDistance(mm(-10), 100)
+    br.driveForDistance(mm(-9), 100)
     br.curve(0, 70, 100)
     br.driveForDistance(mm(36), 100)
 
